eee2e339784749a0adf4f26ec9a46cbd67f6101c
theDwarf595 <theDwarf595@gmail.com>
2025-08-23 18:55:45 +0200

    CI/CD_78

--- app/schemas/onboarding.py
+++ app/schemas/onboarding.py
@@ -1,18 +1,17 @@
 from pydantic import BaseModel
-from datetime import date
 from typing import Optional
 
 
 class OnboardingCreate(BaseModel):
     user_id: int
     name: Optional[str] = None
-    birthdate: Optional[date] = None
+    birthdate: Optional[str] = None
     sex: Optional[str] = None
     height: Optional[int] = None
     weight: Optional[int] = None
     main_goal: Optional[str] = None
     weight_target: Optional[int] = None
-    deadline: Optional[date] = None
+    deadline: Optional[str] = None
     medical_conditions: Optional[str] = None
     sleep_hours: Optional[float] = None
     work_schedule: Optional[str] = None
