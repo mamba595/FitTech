bcb42d268e2d27a2a723df19ec40844544459b0d
theDwarf595 <theDwarf595@gmail.com>
2025-08-23 23:27:52 +0200

    CI/CD_96

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -271,7 +271,7 @@ resource "aws_ecs_service" "api_service" {
     name = "api-service"
     cluster = aws_ecs_cluster.cluster.id
     task_definition = aws_ecs_task_definition.api_task.arn
-    desired_count = 2
+    desired_count = 4
     launch_type = "FARGATE"
 
     network_configuration {
