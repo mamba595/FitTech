c3ae8f9ce210e067b28800589a62057946bb820d
theDwarf595 <theDwarf595@gmail.com>
2025-08-23 18:59:03 +0200

    CI/CD_79

--- app/schemas/onboarding.py
+++ app/schemas/onboarding.py
@@ -5,13 +5,13 @@ from typing import Optional
 class OnboardingCreate(BaseModel):
     user_id: int
     name: Optional[str] = None
-    birthdate: Optional[str] = None
+    birthdate: Optional[Union[str, date]] = None
     sex: Optional[str] = None
     height: Optional[int] = None
     weight: Optional[int] = None
     main_goal: Optional[str] = None
     weight_target: Optional[int] = None
-    deadline: Optional[str] = None
+    deadline: Optional[Union[str, date]] = None
     medical_conditions: Optional[str] = None
     sleep_hours: Optional[float] = None
     work_schedule: Optional[str] = None
