979f61f5842d1215d5fb96845740026f082da504
theDwarf595 <theDwarf595@gmail.com>
2025-08-23 18:22:57 +0200

    CI/CD_74

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -100,5 +100,30 @@ def test_workoutlogs_get(test_user, auth_headers):
     assert resp.status_code == 200
 
 def test_dashboard(test_user, auth_headers):
+    user_id = test_user["id"]
+    data = {
+        "user_id": user_id,
+        "name": "string",
+        "birthdate": "2025-08-21",
+        "sex": "string",
+        "height": 0,
+        "weight": 0,
+        "main_goal": "string",
+        "weight_target": 0,
+        "deadline": "2025-08-21",
+        "medical_conditions": "string",
+        "sleep_hours": 0,
+        "work_schedule": "string",
+        "percFat": 0,
+        "percMuscle": 0,
+        "injuryHist": "string",
+        "expLevel": "string",
+        "restrictedFoods": "string",
+        "timeAvailability": "string",
+        "materialAccess": "string"
+    }
+    resp = requests.post(f"{BASE_URL}/users/{user_id}/onboarding", json=data, headers=auth_headers)
+    assert resp.status_code == 201
+
     resp = requests.get(f"{BASE_URL}/dashboard", headers=auth_headers)
     assert resp.status_code == 200
