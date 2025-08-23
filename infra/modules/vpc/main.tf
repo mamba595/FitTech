153d058e883aaa7280e7530bb5fab2654a0641c4
theDwarf595 <theDwarf595@gmail.com>
2025-08-23 23:06:55 +0200

    CI/CD_95

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -160,9 +160,9 @@ resource "aws_lb_target_group" "lb_tg" {
     health_check {
         path                = "/health"
         interval            = 30
-        timeout             = 5
+        timeout             = 10
         healthy_threshold   = 2
-        unhealthy_threshold = 2
+        unhealthy_threshold = 3
         matcher             = "200-299"    
     }
 
